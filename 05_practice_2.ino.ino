69a67981cdfd6ccce7e2a4c273079fb3ab399294
lee sanghyuk <63362387+tkdgur3207@users.noreply.github.com>
2023-09-19 09:50:00 +0900

    Add files via upload

---
+++ 05_practice_2.ino.ino
@@ -0,0 +1,48 @@
+int led = 7;
+int flag=1;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (flag==1) {
+    digitalWrite(led, LOW);
+    delay(1000);
+
+    digitalWrite(led, HIGH);
+    delay(100);
+    digitalWrite(led, LOW);
+
+    delay(100);
+    
+    digitalWrite(led, HIGH);
+    delay(100);
+    digitalWrite(led, LOW);
+
+    delay(100);
+
+    digitalWrite(led, HIGH);
+    delay(100);
+    digitalWrite(led, LOW);
+
+    delay(100);
+
+    digitalWrite(led, HIGH);
+    delay(100);
+    digitalWrite(led, LOW);
+
+    delay(100);
+
+    digitalWrite(led, HIGH);
+    delay(100);
+    digitalWrite(led, LOW);
+
+    delay(100);
+
+    digitalWrite(led, HIGH);
+    flag=0;
+  }
+}
