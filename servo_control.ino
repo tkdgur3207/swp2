965c2e197031ac8cdabfb046340d91e778fb54de
lee sanghyuk <63362387+tkdgur3207@users.noreply.github.com>
2023-10-17 20:24:15 +0900

    Update servo_control.ino 11P07

--- servo_control.ino
+++ servo_control.ino
@@ -1,5 +1,6 @@
-#include <Servo.h>
+  #include <Servo.h>
 
+// Arduino pin assignment
 #define PIN_LED   9   // LED active-low
 #define PIN_TRIG  12  // sonar sensor TRIGGER
 #define PIN_ECHO  13  // sonar sensor ECHO
@@ -9,13 +10,13 @@
 #define SND_VEL 346.0     // sound velocity at 24 celsius degree (unit: m/sec)
 #define INTERVAL 25      // sampling interval (unit: msec)
 #define PULSE_DURATION 10 // ultra-sound Pulse Duration (unit: usec)
-#define _DIST_MIN 100.0   // minimum distance to be measured (unit: mm)
-#define _DIST_MAX 500.0   // maximum distance to be measured (unit: mm)
+#define _DIST_MIN 180.0   // minimum distance to be measured (unit: mm)
+#define _DIST_MAX 360.0   // maximum distance to be measured (unit: mm)
 
 #define TIMEOUT ((INTERVAL / 2) * 1000.0) // maximum echo waiting time (unit: usec)
 #define SCALE (0.001 * 0.5 * SND_VEL) // coefficent to convert duration to distance
 
-#define _EMA_ALPHA 0.3    // EMA weight of new sample (range: 0 to 1)
+#define _EMA_ALPHA 0.5    // EMA weight of new sample (range: 0 to 1)
                           // Setting EMA to 1 effectively disables EMA filter.
 
 // Target Distance
@@ -25,10 +26,10 @@
 // duty duration for myservo.writeMicroseconds()
 // NEEDS TUNING (servo by servo)
  
-#define _DUTY_MIN 2500 // servo full clockwise position (0 degree)
+#define _DUTY_MIN 600 // servo full clockwise position (0 degree)
 #define _DUTY_NEU 1500 // servo neutral position (90 degree)
-#define _DUTY_MAX 550 // servo full counterclockwise position (180 degree)
-
+#define _DUTY_MAX 2600 // servo full counterclockwise position (180 degree)
+long _DUTY_MAP;
 // global variables
 float  dist_ema, dist_prev = _DIST_MAX; // unit: mm
 unsigned long last_sampling_time;       // unit: ms
@@ -61,30 +62,7 @@ void loop() {
 
   dist_raw = USS_measure(PIN_TRIG, PIN_ECHO); // read distance
 
-//ema필터
   if ((dist_raw == 0.0) || (dist_raw > _DIST_MAX)) {
-    dist_raw = _DIST_MAX + 10.0;    // Set Higher Value
-    digitalWrite(PIN_LED, 1);       // LED OFF
-  } else if (dist_raw < _DIST_MIN) {
-    dist_raw = _DIST_MIN - 10.0;    // Set Lower Value
-    digitalWrite(PIN_LED, 1);       // LED OFF
-  } else {    // In desired Range
-    digitalWrite(PIN_LED, 0);       // LED ON      
-  }
-
-  // Use the previous distance if the current one is out of range
-  if ((dist_raw > _DIST_MAX) || (dist_raw < _DIST_MIN)) {
-    dist_raw = dist_prev;
-  }
-
-  // Update the EMA filter
-  dist_ema = _EMA_ALPHA * dist_raw + (1.0 - _EMA_ALPHA) * dist_ema;
-
-  // Store the current distance as the previous one
-  dist_prev = dist_ema;
-
-
- /* if ((dist_raw == 0.0) || (dist_raw > _DIST_MAX)) {
     dist_raw = dist_prev;           // Cut higher than maximum
     digitalWrite(PIN_LED, 1);       // LED OFF
   } else if (dist_raw < _DIST_MIN) {
@@ -94,23 +72,25 @@ void loop() {
     dist_prev = dist_raw;
     digitalWrite(PIN_LED, 0);       // LED ON      
   }
-  */
 
   // Apply ema filter here  
-  //dist_ema = dist_raw;
+  dist_ema = dist_raw;
+
   // adjust servo position according to the USS read value
-  if(dist_ema<=180) {
-    myservo.writeMicroseconds(_DUTY_MIN);
+ if(dist_ema<=180) {
+    myservo.writeMicroseconds(1000);
   }
-  else if(dist_ema>180 && dist_ema<360) {
-    long _DUTY_MAP = map(_DUTY_MAP,180,360,_DIST_MIN,_DIST_MAX);
+  if(dist_ema>180 && dist_ema<360) {
+    _DUTY_MAP = map(dist_ema,180,360,_DUTY_MIN,_DUTY_MAX);
+    Serial.print(_DUTY_MAP);
     myservo.writeMicroseconds(_DUTY_MAP);
-    if (dist_ema == 190){
-      Serial.println(_DUTY_MAP);
-    }
+    // if (dist_ema >190 && dist_ema<191){
+    //   while(true)
+    //     {Serial.println(_DUTY_MAP);}
+    // }
   }
-  else {
-    myservo.writeMicroseconds(_DUTY_MAX);
+  if (dist_ema >= 360) {
+    myservo.writeMicroseconds(2700);
   }
 
 
@@ -128,7 +108,7 @@ void loop() {
 }
 
 // get a distance reading from USS. return value is in millimeter.
-float USS_measure(int TRIG, int ECHO) 
+float USS_measure(int TRIG, int ECHO)
 {
   digitalWrite(TRIG, HIGH);
   delayMicroseconds(PULSE_DURATION);
